9836f32f0e58355036730dfe3cc15cc154d36c3e
samfromdeath <samuel@intelogy.com.au>
2015-11-04 18:58:38 +1100

    Added New Structures

---
+++ DictatorRTS/Entities/Structures/Bridge.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictatorRTS.Entities.Structures
+{
+    public class Bridge : Structure
+    {
+
+    }
+}
