9df4fd2e4129c686731239852df6d752df2125aa
samfromdeath <samuel@intelogy.com.au>
2015-11-04 19:04:20 +1100

    LifeForms

---
+++ DictatorRTS/Entities/LifeForms/LifeForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictatorRTS.Entities.LifeForms
+{
+    public class LifeForm : BaseEntity
+    {
+
+    }
+}
