d547cb14611a6f5a9c1ad1448723a0d01087197a
samfromdeath <samuel@intelogy.com.au>
2015-11-04 16:43:31 +1100

    Renamed a File

--- DictatorRTS/Program.cs
+++ DictatorRTS/Program.cs
@@ -14,7 +14,7 @@ namespace DictatorRTS
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Client())
                 game.Run();
         }
     }
