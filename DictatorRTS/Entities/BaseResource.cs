1b44208e808e7a403f987910f8e8f831f7608df9
bLeenstra <brendan.leenstra@gmail.com>
2015-11-04 19:39:55 +1100

    Currency

---
+++ DictatorRTS/Entities/BaseResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictatorRTS.Entities {
+    class BaseResource
+    {
+        private decimal _amount;
+
+        public BaseResource(decimal amount)
+        {
+            this._amount = amount;
+        }
+
+        public decimal Amount { get; set; }
+    }
+}
---
+++ DictatorRTS/Entities/Resources/Currency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictatorRTS.Entities.Resources {
+    class Currency : BaseResource {
+        private decimal _buyStrength;
+        private readonly string _name;
+        public Currency(string name, decimal amount) : this(name, amount, 0m) {
+
+        }
+
+        public Currency(string name, decimal amount, decimal buy) : base(amount) {
+            _name = name;
+            _buyStrength = buy;
+        }
+
+        /// <summary>
+        ///     Returns the buying power of this currency
+        /// </summary>
+        public decimal BuyStrength {
+            get {
+                return _buyStrength;
+            }
+            set {
+                _buyStrength = value;
+            }
+        }
+
+        public string Name {
+            get { return _name;}
+        }
+
+        
+    }
+}
