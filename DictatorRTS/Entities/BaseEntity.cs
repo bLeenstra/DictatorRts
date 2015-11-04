7231568012b2ebec4e62abdaca7f907648be05a7
samfromdeath <samuel@intelogy.com.au>
2015-11-04 16:46:10 +1100

    Base Directories

---
+++ DictatorRTS/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictatorRTS.Entities
+{
+    /// <summary>
+    /// This will be used for every base class we have in the program.
+    /// </summary>
+    public class BaseEntity
+    {
+    }
+}
