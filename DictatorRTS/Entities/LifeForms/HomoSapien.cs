6ae17a03e750a05abd13428c9238b7b4e8d4420d
bLeenstra <brendan.leenstra@gmail.com>
2015-11-08 19:45:04 +1100

    Forgot to add in previous commit

---
+++ DictatorRTS/Entities/LifeForms/HomoSapien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using DictatorRTS.GlobalFunctions;
+
+namespace DictatorRTS.Entities.LifeForms
+{
+    class HomoSapien : Theria {
+        private string _name;
+
+        public HomoSapien(string name) : this(name, (Gender)EnumFunctions.GetRandomEnumElement(default(Gender)), IntegerFunctions.GetRandomInteger(350, 500), IntegerFunctions.GetRandomInteger(2000, 5000)) {
+        }
+
+        public HomoSapien(string name, Gender sex, int height, int weight) : base(sex, height, weight) {
+            this._name = name;
+        }
+
+        public HomoSapien(string name, Gender sex, int height, int weight, DateTime dateOfBirth) : base(sex, height, weight, dateOfBirth) {
+            this._name = name;
+        }
+
+        public string Name {
+            get {
+                return _name;
+            }
+            set {
+                _name = value;
+            }
+        }
+    }
+}
---
+++ DictatorRTS/Entities/LifeForms/Theria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DictatorRTS.Entities.LifeForms {
+    /// <summary>
+    ///     https://en.wikipedia.org/wiki/Theria
+    ///     The subsection of mammals that give birth rather than lay eggs
+    /// </summary>
+    class Theria : Metazoa {
+        private bool _pregnant;
+
+        internal Theria(Gender sex, decimal height, decimal weight) : base(sex, height, weight) {
+            this._pregnant = false;
+        }
+
+        internal Theria(Gender sex, decimal height, decimal weight, DateTime dateofBirth) : base(sex, height, weight, dateofBirth) {
+            this._pregnant = false;
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        public bool Pregnant {
+            get {
+                return _pregnant;
+            }
+            set {
+                if( Sex == Gender.Female ) {
+                    _pregnant = value;
+                }
+            }
+        }
+    }
+}
